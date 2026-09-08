aa9ed1d808596da9f7d25febb8bd8b274ac09a46
kaleeswaran400 <kaleeswaran169916@gmail.com>
2026-09-08 14:38:42 +0530

    rough

--- Budget Optimation Dashboard/campaing optimization analysis.sql
+++ Budget Optimation Dashboard/campaing optimization analysis.sql
@@ -1,5 +1,5 @@
-/* Which campaigns consume the highest budget?*/
-/*
+-- Which campaigns consume the highest budget?*/
+
 with sum_spend as(
  select sum(Planned_Daily_Ad_Set_Budget) as sum_spend_per, Campaign_Name
 from marketing_campaign_analysis_2_5_years
@@ -8,12 +8,11 @@ group by Campaign_Name)
 select round(sum_spend_per,2) as mx_spend,Campaign_Name,rank() over( order by sum_spend_per desc) as rank_per_budget
 from sum_spend
 order by mx_spend  desc
-limit 10;*/
+limit 10;
+ 
+-- Which campaigns have the highest ROAS?
 
-/* 
-Which campaigns have the highest ROAS?
-*/
-/*
+
 with request_data1 as (
 select  Campaign_Name,Ad_Set_Name, Ad_Name, sum(ROAS)
 from marketing_campaign_analysis_2_5_years
@@ -30,9 +29,8 @@ from requested_data2
 where rank_per_ROAS<6;*/
 
 
-/*Which campaigns have the lowest CPA?
-*/
-/*
+--Which campaigns have the lowest CPA?
+
 with requested_data as
 (select Campaign_Name,Ad_Set_Name,Ad_Name,CPA , row_number() over(partition by Campaign_Name order by CPA) as rank_per_CPA
 from marketing_campaign_analysis_2_5_years
@@ -40,20 +38,20 @@ where CPA !=0)
 select Campaign_Name,Ad_Set_Name,Ad_Name,CPA
 from requested_data
 where rank_per_CPA<6;
-*/
 
 
-/* above is not a optimum strategy to find the low roas*/
 
-/* 
+-- above is not a optimum strategy to find the low roas*/
+
+ 
 from marketing_campaign_analysis_2_5_years)
 select Campaign_Name,Ad_Set_Name,CPA
 from quatail_cet
 where qurtail =1;/* 
 
-/*Which campaigns are under-spending?*/
+--Which campaigns are under-spending?*/
 
-/*
+
 with requested_data as
 (select Campaign_Name,Ad_Set_Name,Ad_Name, round(((Planned_Daily_Ad_Set_Budget-Actual_Spend)/Planned_Daily_Ad_Set_Budget)*100,2) as spending_prct
 from marketing_campaign_analysis_2_5_years),
@@ -66,8 +64,8 @@ from requested_data2
 where rank_per_spending <50;*/
 
 
-/*Which campaigns exceed their planned budget*/
-/*with requested_data as
+--Which campaigns exceed their planned budget*/
+with requested_data as
 (select Campaign_Name,Ad_Set_Name,Ad_Name, round(((Planned_Daily_Ad_Set_Budget-Actual_Spend)/Planned_Daily_Ad_Set_Budget)*100,2) as spending_prct
 from marketing_campaign_analysis_2_5_years),
 requested_data2 as(
@@ -78,18 +76,17 @@ select Campaign_Name,Ad_Set_Name,Ad_Name, spending_prct
 from requested_data2
 where rank_per_spending <50;*/
 
-/*  What is budget utilization by campaign?    */
-/*
+-- What is budget utilization by campaign?    
+
 select Campaign_Name,Ad_Set_Name, Ad_Name,(sum(Actual_Spend)/sum(Planned_Daily_Ad_Set_Budget))*100 as utilization
 from marketing_campaign_analysis_2_5_years
-group by Campaign_Name,Ad_Set_Name,Ad_Name;*/
+group by Campaign_Name,Ad_Set_Name,Ad_Name;
+
+--How does ROAS change month-over-month? 
 
 
-/* How does ROAS change month-over-month? */
+-- update the date in a corrt format
 
-
-/*update the date in a corrt format*/
-/*
 ALTER TABLE marketing_campaign_analysis_2_5_years
 ADD COLUMN Date_temp DATE;
 
@@ -100,23 +97,23 @@ set months=str_to_date(Date, '%d/%m/%Y');
 SET SQL_SAFE_UPDATES = 1;*/
 
 
-/*
+
 with previous_data as (select Campaign_Name, ROAS, Date, Ad_Set_Name,Ad_Name, lag(ROAS,1) over(partition by  Campaign_Name order by months) as previous_roas
 from marketing_campaign_analysis_2_5_years)
 select Campaign_Name, ROAS,previous_roas, Date,Ad_Set_Name,Ad_Name,(ROAS-previous_roas)/previous_roas as diff_roas
 from previous_data;*/
 
-/*Which /* campaigns are under-spending?*/
-/*select Campaign_Name, (Planned_Daily_Ad_Set_Budget-Actual_Spend)/Planned_Daily_Ad_Set_Budget
-from marketing_campaign_analysis_2_5_years;*/
+-- Which /* campaigns are under-spending?
+select Campaign_Name, (Planned_Daily_Ad_Set_Budget-Actual_Spend)/Planned_Daily_Ad_Set_Budget
+from marketing_campaign_analysis_2_5_years;
 
-/*Which campaigns generate the most conversions per ₹1,000?*/
-/*
+-- Which campaigns generate the most conversions per ₹1,000?
+
 select Campaign_Name,Ad_Set_Name,Ad_Name, (Actual_Spend/Impressions)*1000 as CPM
 from marketing_campaign_analysis_2_5_years;
-*/
-/*Which campaigns have high spend but poor conversion performance?*/
-/*
+
+-- Which campaigns have high spend but poor conversion performance?*/
+
 with requireed_data as(
 select  Campaign_Name,Ad_Set_Name,Ad_Name,Actual_Spend,Conversions, row_number() over(partition by Campaign_Name order by Conversions) as con_rank, row_number() over(partition by Campaign_Name order by Actual_Spend desc) as spend_con
 from marketing_campaign_analysis_2_5_years)
@@ -124,19 +121,16 @@ select Campaign_Name,Ad_Set_Name,Ad_Name,Actual_Spend,Conversions, row_number() 
 from requireed_data
 order by top_campaign 
  
- */
- /*Which campaigns have high ROAS but low spending?*/
- /*
+-- Which campaigns have high ROAS but low spending?
+
  with requireed_data as(
 select  Campaign_Name,Ad_Set_Name,Ad_Name,Actual_Spend,ROAS, row_number() over(partition by Campaign_Name order by ROAS desc) as con_rank, row_number() over(partition by Campaign_Name order by Actual_Spend ) as spend_con
 from marketing_campaign_analysis_2_5_years)
 select Campaign_Name,Ad_Set_Name,Ad_Name,Actual_Spend,ROAS,con_rank,spend_con ,row_number()over( order by con_rank+spend_con ) as top_campaign
 from requireed_data
-order by top_campaign */
+order by top_campaign 
  
- /* What would happen if 10% of the budget were shifted from low-performing campaigns to high-performing campaigns?*/
- 
- /*
+ -- What would happen if 10% of the budget were shifted from low-performing campaigns to high-performing campaigns?*/
  
  with fetch_data as
  (select Campaign_Name,Ad_Set_Name,Ad_Name,Actual_Spend,CPA,Revenue,Conversions,Planned_Daily_Ad_Set_Budget
@@ -145,10 +139,8 @@ order by top_campaign */
  select Campaign_Name,Ad_Set_Name,Ad_Name, CPA,(0.10*Planned_Daily_Ad_Set_Budget)/CPA as add_CPA,0.10*Planned_Daily_Ad_Set_Budget as n_CPA
  from fetch_data;
  
- */
- 
- /*What is the marginal CPA at different spending levels?*/
- /*
+-- What is the marginal CPA at different spending levels?
+
  with cte_data as (select Campaign_Name,Ad_Set_Name,Ad_Name, Date, CPA, Actual_Spend,  lag(CPA,1) over(partition by Ad_Name order by Ad_Name) as pre_CPA,
  lag(Actual_Spend,1) over(partition by Ad_Name order by Ad_Name) as pre_spend
  from marketing_campaign_analysis_2_5_years
@@ -157,31 +149,29 @@ order by top_campaign */
  from cte_data
  order by marginal_CPA DESC; */
  
- /*Which campaigns should receive additional budget? */
+ -- Which campaigns should receive additional budget? 
  
- /* SIS is curated so it is onot accurate result*/
- /*
+ -- SIS is curated so it is onot accurate result
+ 
  with request_data as (select Campaign_Name,Ad_Set_Name,Ad_Name, (`Target CPA`/CPA)*(100-SIS) as scalibility_score
  from marketing_campaign_analysis_2_5_years)
  select *
  from request_data
  where scalibility_score is not null
  order by scalibility_score
- */
  
- /*Which campaigns should have their budgets reduced? */
-  
-  /*
+ -- Which campaigns should have their budgets reduced? 
+ 
   with request_data as (select Campaign_Name,Ad_Set_Name,Ad_Name, (CPA/`Target CPA`) as CPA_deficit
  from marketing_campaign_analysis_2_5_years)
  select *
  from request_data
  where CPA_deficit <1
- order by CPA_deficit
- */
+ order by CPA_deficit 
+
  
- /*Which campaigns are scalable and efficient? */
- /*
+ Which campaigns are scalable and efficient? 
+ 
  with request_data as(
  select Campaign_Name,Ad_Set_Name,Ad_Name, Actual_Spend,CPA,(Actual_Spend/SIS) as max_scalable_buget
  from marketing_campaign_analysis_2_5_years),
@@ -192,7 +182,7 @@ order by top_campaign */
  select Campaign_Name,Ad_Set_Name,Ad_Name,additional_con*CPA as revenue
  from request_ata_3*/
  
- /*Which campaigns consistently outperform their historical benchmarks? */
+ Which campaigns consistently outperform their historical benchmarks? 
  
  with request_data as(
  select Campaign_Name,Ad_Set_Name,Ad_Name,CPA,ROAS, lag(CPA,1) over(partition by Ad_Name order by Date) as previous_CPA, lag(ROAS,1) over(partition by Ad_Name order by Date) as previous_ROAS
